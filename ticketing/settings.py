53e2f8bd396671666e90e199c923f00e3d6d710d
Aditya Sinha <adityakumarsinha2014@gmail.com>
2020-03-22 19:15:01 +0530

    Update allowed hosts

--- ticketing/settings.py
+++ ticketing/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'complaints-portal.herokuapp.com']
 
 # Application definition
 INSTALLED_APPS = [
